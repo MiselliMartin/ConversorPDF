9c0a67d51679745562d14e349e4d57451e842970
Martín Miselli <129102787+MiselliMartin@users.noreply.github.com>
2023-05-12 19:58:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ app = Flask(__name__)
 CORS(app)
 
 @app.after_request
-def add_cors_header(response):
-    response.headers['Access-Control-Allow-Origin'] = 'https://benevolent-rolypoly-23a200.netlify.app'
-    response.headers.add('Access-Control-Allow-Methods', 'POST')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Origin')
+def add_cors_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
 @app.route("/convert", methods=["POST"]) #POST
