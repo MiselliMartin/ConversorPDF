0fabbd0d2a52f8db0bb67febf702da9971b63ec3
Martín Miselli <129102787+MiselliMartin@users.noreply.github.com>
2023-05-12 20:02:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ CORS(app)
 @app.after_request
 def add_cors_headers(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
     return response
+
 
 @app.route("/convert", methods=["POST"]) #POST
 def convert():
